cd9e0eb65a0b75f0a19c08fe338c36150835cc96
tanghc <tanghc@xweisoft.com>
2025-08-26 14:41:35 +0800

    chore: 开启webpack缓存持久化

--- config/index.ts
+++ config/index.ts
@@ -44,7 +44,7 @@ export default defineConfig<"webpack5">(async (merge, { command, mode }) => {
       },
     },
     cache: {
-      enable: false, // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+      enable: true, // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
     },
     mini: {
       postcss: {
